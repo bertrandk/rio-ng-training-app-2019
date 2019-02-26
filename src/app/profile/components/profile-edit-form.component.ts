d5b51c6577c55c69cb8f95d6685166adade2204e
Evan Schultz <evan@rangle.io>
2019-02-26 13:38:51 -0500

    solution

--- src/app/profile/components/profile-edit-form.component.ts
+++ src/app/profile/components/profile-edit-form.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ValidatorFn
+} from '@angular/forms';
 
+function confirmEmail(formGroup: FormGroup): Validators {
+  const a = formGroup.get('email').value;
+  const b = formGroup.get('confirmEmail').value;
+  return a === b
+    ? null
+    : {
+        confirmEmail: {
+          message: `expected email and confirm email to match`,
+          email: a,
+          confirmEmail: b
+        }
+      };
+}
 @Component({
   selector: 'app-profile-edit-form',
   templateUrl: './profile-edit-form.component.html',
@@ -10,17 +28,24 @@ export class ProfileEditFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
   form: FormGroup;
   ngOnInit() {
-    this.form = this.fb.group({
-      id: [],
-      image: [],
-      languageId: [],
-      firstName: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      averageNumberOfHoursPerDay: [
-        '',
-        [Validators.required, Validators.min(0), Validators.max(24)]
-      ]
-    });
+    this.form = this.fb.group(
+      {
+        id: [],
+        image: [],
+        languageId: [],
+        firstName: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        averageNumberOfHoursPerDay: [
+          '',
+          [Validators.required, Validators.min(0), Validators.max(24)]
+        ],
+        email: [],
+        confirmEmail: []
+      },
+      {
+        validators: confirmEmail
+      }
+    );
   }
   public onSubmit() {
     console.log('proper');
